926839ffe952f6065a05ace60a18716358d432a9
Vavarooks <vavarooks123@gmail.com>
2020-02-27 13:34:13 -0800

    changes somethings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,14 @@
 const questions = [
-  
+
+
 ];
 
 function writeToFile(fileName, data) {
- 
+    // As a <role> I can <capability>, so that <receive benefit>
+    // In order to <receive benefit> as a <role>, I can <goal/desire>
+    // As <who> <when> <where>, I <want> because <why>
 }
 
 function init() {
 
-init();
+init();}
