60fa2deadd29a3b6615323cd1510a985280560eb
Vavarooks <vavarooks123@gmail.com>
2020-02-27 15:11:11 -0800

    adding files
    Signed-off-by: Vavarooks <vavarooks123@gmail.com>

---
+++ index.js
@@ -0,0 +1,65 @@
+var  inquirer = require("inquirer");
+
+var axios = require("axios");
+
+var fs = require("fs");
+
+var pdf = require("html-pdf");
+
+var generatehtml = require("./assets/js/generateHtml")
+var usercolor = "";
+
+const questions = [
+    {type:"input",
+      name:"github",
+      message:"Enter Gihub Username?"},
+      {type:"list",
+      choices: ["red","blue", "yellow", "green", "purple", "orange"],
+      name:"favColor",
+      message:"Enter favorite color?"}
+
+];
+
+function writeToFile(data) {
+  fs.writeFileSync("index.html", data)
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then(function(response){
+        usercolor = response.favColor
+        console.log(response)
+        return axios.get(`https://api.github.com/users/${response.github}`)
+    })
+    .then(function(apiresult){
+        console.log(apiresult.data)
+        var userdata = {
+            color: usercolor,
+            username: apiresult.data.login,
+            name: apiresult.data.name,
+            followers: apiresult.data.followers,
+            following: apiresult.data.following,
+            publicRepo: apiresult.data.public_repos,
+            bio: apiresult.data.bio,
+            image: apiresult.data.avatar_url,
+            blog: apiresult.data.blog,
+            location: apiresult.data.location
+        }
+        return generatehtml(userdata);
+    })
+    .then(function(html){
+        console.log(html);
+        var options = { format: 'Letter'};
+
+        writeToFile(html);
+        var htmlfile= fs.readFileSync("./index.html", "utf8")
+        pdf.create(htmlfile, options).toFile("./profile.pdf", function(error, response){
+            if (error){
+                throw error
+            }
+            console.log("pdf file created")
+        })
+    })
+}
+
+init();
